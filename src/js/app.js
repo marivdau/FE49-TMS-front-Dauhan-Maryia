b2943e40ad5bcaeeb22db39d6a6c5dd46ada8eec
marivdau <87074474+marivdau@users.noreply.github.com>
2023-05-03 12:39:52 +0300

    fix of styles, add images

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,7 @@
 const swiper = new Swiper('.swiper', {
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
-  },
-  initialSlide: 2,
+  }
 });
